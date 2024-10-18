0add8f346933a187424b762b6bfaec9cf5c96e63
dhaneshdhkarey <dhaneshdhakrey@gmail.com>
2024-10-18 22:53:52 +0530

    3rd Push
    Live share vs code

---
+++ src/Components/MainDIv/FoodCont.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FoodCont(){
+   
+    return (
+        <div>
+              THis is contaier WHich will contain Food
+        </div>
+    )
+}
+
+export default FoodCont;
--- src/Components/MainDIv/MainCont.js
+++ src/Components/MainDIv/MainCont.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
 import FilterCont from "./FilterCont";
+import FoodCont from "./FoodCont";
+
 function MainCont(){
     return (
         <React.Fragment>
             <Navbar/>
             <FilterCont/>
+            <FoodCont/>
         </React.Fragment>
     )
 }
