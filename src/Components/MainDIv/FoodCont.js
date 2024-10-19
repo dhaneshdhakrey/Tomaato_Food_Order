485e4327b7cb51d34ac54719a8bb12aefa9def11
Shivam Kataria <133666415+shivamkataria7622@users.noreply.github.com>
2024-10-19 22:43:03 +0530

    test1 raat

--- src/Components/MainDIv/FoodCont.js
+++ src/Components/MainDIv/FoodCont.js
@@ -3,7 +3,7 @@ import React from 'react';
 function FoodCont(){
    
     return (
-        <div className='border border 2px solid black '>
+        <div className='border border 2px solid bg-green-500 '>
               THis is contaier WHich will contain Food
               <div> image val ad to cart</div>
               <div> menu add to cart vala</div>
