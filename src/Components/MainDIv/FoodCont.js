67c82912984990aee974c01912cca6cdd6555ce6
dhaneshdhkarey <dhaneshdhakrey@gmail.com>
2024-10-19 23:06:13 +0530

    19 october last push
    CHanges in git ignore file
    ignoring json files

--- src/Components/MainDIv/FoodCont.js
+++ src/Components/MainDIv/FoodCont.js
@@ -3,7 +3,9 @@ import React from 'react';
 function FoodCont(){
    
     return (
+        
         <div className='border border 2px solid bg-green-500 '>
+            
               THis is contaier WHich will contain Food
               <div> image val ad to cart</div>
               <div> menu add to cart vala</div>
