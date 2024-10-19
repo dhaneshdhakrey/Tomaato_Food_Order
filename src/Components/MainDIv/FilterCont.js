ebcdfa4f346465b5c3eea5535d186292941e210c
Shivam Kataria <133666415+shivamkataria7622@users.noreply.github.com>
2024-10-19 21:36:39 +0530

    19 oct
    Nav bar +filter done

--- src/Components/MainDIv/FilterCont.js
+++ src/Components/MainDIv/FilterCont.js
@@ -1,12 +1,18 @@
 import React from "react";
+const BUTTON_CLASS = "bg-secondary text-secondary-foreground hover:bg-secondary/80 px-4 py-2 rounded-lg border border-border";
 
 function FilterCont(){
     return (
-        <div classes='flter continer'>
-            
-            <div class='Actual filter'></div>
-            <div clas>Veg/Non Veg</div>
-            <div class ='cousines'>Select Cousines</div>
+        <div className='flter continer flex justify-center flex-row gap-3 ml-[-56.25rem]'>
+            <button className={BUTTON_CLASS}>Filters</button>
+            <div className ='Actual filter'></div>
+            <button className={BUTTON_CLASS}>Pure veg</button>
+            <select className="border border-border rounded-lg p-2 bg-white">
+                    <option>Cuisines</option>
+                    <option>Indian</option>
+                    <option>Chinese</option>
+                    <option>Italian</option>
+                </select>
         </div>
     )
 }
--- src/Components/MainDIv/Navbar.js
+++ src/Components/MainDIv/Navbar.js
@@ -1,20 +1,37 @@
-import React from "react";
-import SearchBar from "./SearchBar";
-import classes from './Navbar.module.css';
+import React from 'react';
 
-function Navbar() {
+const INPUT_CLASS = "border border-border rounded-lg p-4 w-1/3 shadow-lg";
+const BUTTON_CLASS = "bg-secondary text-secondary-foreground hover:bg-secondary/80 px-4 py-2 rounded-lg border border-border";
+const TEXT_CLASS = "text-muted-foreground border border-border rounded-lg  bg-white";
+
+const Header = () => {
     return (
-        <div className={classes.Navbar}>
-            <span>Tomaato</span>
-            <div className={classes.SearchBar}>
-                <SearchBar />
-            </div>
+        <div className="flex flex-col items-center justify-center p-4 bg-background mx-10 gap-6">
+            <div className="flex items-center space-x-4 w-full justify-center ">
+                <span className='font-bold text-3xl'>TOMAATO</span>
+                <span className='border border-border rounded-lg p-4 shadow-lg'>Dinnedevarapadu, Q2J</span>
+            <input className={INPUT_CLASS} type="text" placeholder="Search for restaurant, cuisine or dish" />
             <div>
-                <button>Sign In</button>
-                shivam
+                <img src='https://b.zmtcdn.com/images/user_avatars/mug_2x.png?fit=around%7C100%3A100&crop=100%3A100%3B%2A%2C%2A' className='w-12 h-12' alt='pic'></img>
             </div>
+            <span>Shivam</span>
+            <select className={TEXT_CLASS}>
+                <option></option>
+                <option>profile</option>
+                <option>language</option>
+                <option>select</option> 
+                <option>Telugu</option>
+            </select>
+            </div>
+            <div className="flex items-center space-x-4 mt-4 ml-[-770px]" > {/* Moved this below the search bar */}
+               
+               
+               
+            </div>
+            
+           
         </div>
     );
-}
+};
 
-export default Navbar;
+export default Header;
