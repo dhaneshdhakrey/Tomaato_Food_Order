57d66eda9d24445468619db11ebd5c23bb55c986
dhaneshdhkarey <dhaneshdhakrey@gmail.com>
2024-10-20 16:50:03 +0530

    speed test push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {},
   },
